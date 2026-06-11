d79ed15910120fa4a3405318e0e89bc22efa2d84
Jenna Eizadi <jeizadi@gmail.com>
2026-06-10 20:29:03 -0700

    Modified main to attemptto connect to GarminBD

--- .fnPCOS/main.py
+++ .fnPCOS/main.py
@@ -1,8 +1,16 @@
-from garmin_fit_sdk import Decoder, Stream
+import garmindb
+print(dir(garmindb))
 
-stream = Stream.from_file("Activity.fit")
-decoder = Decoder(stream)
-messages, errors = decoder.read()
+from garmindb import GarminDb, MonitoringDb, ActivitiesDb, GarminSummaryDb
+from garmindb.garmindb import MonitoringHeartRate, DailySummary, Sleep, Weight
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import date, timedelta
 
-print(errors)
-print(messages)
+# Point to your DB directory (default location)
+db_params = garmindb.ConfigManager.get_db_params()
+
+garmin_db = GarminDb(db_params)
+monitoring_db = MonitoringDb(db_params)
+summary_db = GarminSummaryDb(db_params)
+activities_db = ActivitiesDb(db_params)
